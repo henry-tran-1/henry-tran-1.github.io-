5e142825e0fab07b64bd81ea1a56e9fb242ff523
Henry Tran <henrytran089@gmail.com>
2024-12-21 13:21:49 +1300

    add icons to links bar

--- client/components/Links.tsx
+++ client/components/Links.tsx
@@ -1,10 +1,28 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 export default function Links() {
   return (
-    <div>
+    <div className="ml-10">
       <section className="flex justify-end gap-4 font-body font-thin">
-        <button>Github</button>
-        <button>Linkedin</button>
-        <button>Email</button>
+        <a href="mailto:henrytran089@gmail.com">
+          <FontAwesomeIcon icon={faEnvelope} className="text-2xl" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/henry-tran-dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} className="text-2xl" />
+        </a>
+        <a
+          href="https://github.com/henry-tran-1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} className="text-2xl" />
+        </a>
       </section>
     </div>
   )
