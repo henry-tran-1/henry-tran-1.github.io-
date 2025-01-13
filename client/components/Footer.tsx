3d52cff8d24c9d8e70d56ee7ec9ec5ab10e83f27
Henry Tran <henrytran089@gmail.com>
2025-01-14 08:40:39 +1300

    fix footer colours

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,13 +1,44 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Links from './Links'
+
 import Navbar from './Navbar'
 import { faCopyright } from '@fortawesome/free-regular-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default function Footer() {
   return (
     <div className="flex h-32 items-center justify-between bg-amber-400">
       <div className="ml-10">
-        <Links />
+        <div>
+          <section className="flex gap-4 font-body font-thin">
+            <a href="mailto:henrytran089@gmail.com">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="text-2xl duration-100 hover:text-3xl"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/henry-tran-dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className="text-2xl duration-100 hover:text-3xl"
+              />
+            </a>
+            <a
+              href="https://github.com/henry-tran-1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                className="text-2xl duration-100 hover:text-3xl"
+              />
+            </a>
+          </section>
+        </div>
       </div>
       <div className="flex items-center">
         <FontAwesomeIcon icon={faCopyright} className="m-2" />
